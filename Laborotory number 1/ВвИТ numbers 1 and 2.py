e39e88a9cdedb967b65073886c6572f0e12f0eee
fazzilka <admiral.kanaris3@gmail.com>
2025-01-13 16:13:05 +0300

    Laborotory

---
+++ Laborotory number 1/ВвИТ numbers 1 and 2.py
@@ -0,0 +1,11 @@
+#Number 1
+num = int(input("Введите число: "))
+print(int(i) for i in range(1, num + 1))
+
+#Number 2
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+if num1 > num2:
+    print('Большее число:', num1)
+else:
+    print('Болльшее число:', num2)
